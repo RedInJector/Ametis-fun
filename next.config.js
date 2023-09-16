ac53cbb43a8257ee9c8df7c9a1e332abf0b77226
RedInJector <imaxprots@gmail.com>
2023-09-16 19:55:08 +0200

    prod v2.0.0

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {images: {
-    domains: ['cdn.discordapp.com', 'mc-heads.net', 'localhost', 'cdn.pixabay.com'],
+    domains: ['cdn.discordapp.com', 'mc-heads.net', 'localhost', 'cdn.pixabay.com', 'www.ametis.xyz'],
   },}
 
 module.exports = nextConfig
